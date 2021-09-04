8de76d251883d9613581a6cdb193e2a314d34d87
Chris Griffis <chrisiffis@gmail.com>
2021-09-04 05:35:08 -0700

    Changed 4 spaces to 2 and added remove '.' func

--- Batch Find Replace.py
+++ Batch Find Replace.py
@@ -6,9 +6,10 @@ import sys, getopt
 # This is a python script to find and replace file names in a
 # given directory.
 
-DIRECTORY       = "E:\TV Shows\Warrior\Season 1"   
+DIRECTORY       = "E:\Downloads\Its.Always.Sunny.In.Philadelphia.S13.1080p.AMZN.WEBRip.DDP5.1.x264-NTb[rartv]"   
 FIND            = "[Judas] Highschool of the Dead - "
 REPLACE         = "Highschool of the Dead S01E"
+REMOVE_PERIODS  = True
 
 # number of files to be renamed
 NUM_START       = 1
@@ -30,87 +31,110 @@ def fixPath(path):
       newPath += c
   return newPath
 
+def checkDirectory(path):
+  if not os.path.isdir(path):
+    print("ERROR: \""+path+"\" cannot be found")
+    exit()
+  return True
+
 def batchFileFindandReplace(n=""):
-    fileDir = DIRECTORY + n
-    filesRenamed = 0
+  fileDir = DIRECTORY + n
+  filesRenamed = 0
+  checkDirectory(fileDir)
+  
+  print("\nDirectory: \""+fileDir+"\"\n")
 
-    if not os.path.isdir(fileDir):
-        print("ERROR: \""+fileDir+"\" cannot be found")
-        exit()
-    print("\nDirectory: \""+fileDir+"\"\n")
+  index = NUM_START
+  for fileName in os.listdir(fileDir):
+    if NUM_END and index > NUM_END:
+      break
+    foundIndex = fileName.find(FIND)
+    if foundIndex == -1:                 # If not found
+      continue
 
-    index = NUM_START
-    for fileName in os.listdir(fileDir):
-        if NUM_END and index > NUM_END:
-            break
-        foundIndex = fileName.find(FIND)
-        if foundIndex == -1:                 # If not found
-            continue
-
-        nextChar = foundIndex + len(FIND)# Finds the next character after text
+    nextChar = foundIndex + len(FIND)# Finds the next character after text
  
-        if nextChar >= len(fileName):   # If text found at the end of the filename
-            newName = fileName[:foundIndex] + REPLACE
-        else:
-            newName = fileName[:foundIndex] + REPLACE + fileName[nextChar:]
+    if nextChar >= len(fileName):   # If text found at the end of the filename
+      newName = fileName[:foundIndex] + REPLACE
+    else:
+      newName = fileName[:foundIndex] + REPLACE + fileName[nextChar:]
 
-        if not DEBUG:
-            os.rename(fileDir+"/"+fileName, fileDir+"/"+newName)
-        print(fileName + " --> " + newName)
-        filesRenamed += 1
-        index += 1
-    return filesRenamed
+    if not DEBUG:
+      os.rename(fileDir+"/"+fileName, fileDir+"/"+newName)
+    print(fileName + " --> " + newName)
+    filesRenamed += 1
+    index += 1
+  return filesRenamed
 
 def introMessage():
-    print("\n================================================================================")
-    print("             Starting Batch File Name Find and Replace")
-    print("================================================================================")
+  print("\n================================================================================")
+  print("             Starting Batch File Name Find and Replace")
+  print("================================================================================")
 
 
 def endingMessage(filesRenamed=0):
-    print()
-    if filesRenamed == 0:
-        print("No files found with \""+FIND+"\" Did you mean something else?")
+  print()
+  if filesRenamed == 0:
+    print("No files found with \""+FIND+"\" Did you mean something else?")
+  else:
+    if DEBUG:
+      print(str(filesRenamed) + " files to be renamed")
     else:
-        if DEBUG:
-            print(str(filesRenamed) + " files to be renamed")
-        else:
-            print(str(filesRenamed) + " files renamed")
+      print(str(filesRenamed) + " files renamed")
+  print("================================================================================\n")
 
 def default():
-    filesRenamed = batchFileFindandReplace()
-    endingMessage(filesRenamed)
-    print("================================================================================\n")
+  filesRenamed = batchFileFindandReplace()
+  endingMessage(filesRenamed)
+  
 
 def numberedDirectory():
-    totalFilesRenamed = 0
-    for num in range(NUM_START, NUM_END+1):
-        if NUM_END > 9:
-            number = str(num).zfill(2)
-        else:
-            number = str(num)
-        filesRenamed = batchFileFindandReplace(number)
-        endingMessage(filesRenamed)
-        print("\n")
-        totalFilesRenamed += filesRenamed
-    print(str(totalFilesRenamed)+" files renamed in total")
-    print("================================================================================\n")
+  totalFilesRenamed = 0
+  for num in range(NUM_START, NUM_END+1):
+    if NUM_END > 9:
+      number = str(num).zfill(2)
+    else:
+      number = str(num)
+    filesRenamed = batchFileFindandReplace(number)
+    endingMessage(filesRenamed)
+    print("\n")
+    totalFilesRenamed += filesRenamed
+  print(str(totalFilesRenamed)+" files renamed in total")
+  print("================================================================================\n")
+
+def removePeriods():
+  filesRenamed = 0
+  fileDir = DIRECTORY
+  checkDirectory(fileDir)
+
+  for fileName in os.listdir(fileDir):
+    temp = fileName.split('.')
+    newName = ' '.join(temp[:-1]) + '.' + temp[-1]
+	
+    if not DEBUG:
+      os.rename(fileDir+"/"+fileName, fileDir+"/"+newName)
+    print(fileName + " --> " + newName)
+    filesRenamed += 1
+  endingMessage(filesRenamed)
+
 
 def main():
-    opts, args = getopt.getopt(sys.argv[1:], "wt")
-    for opt, arg in opts:
-        if opt == '-w':
-            global DEBUG
-            DEBUG = False
-    introMessage()
-    if not DEBUG:
-        print("Confirm Rename")
-        input()
-    if NUM_DIR:
-        numberedDirectory()
-    else:
-        default()
+  opts, args = getopt.getopt(sys.argv[1:], "wt")
+  for opt, arg in opts:
+    if opt == '-w':
+      global DEBUG
+      DEBUG = False
+  introMessage()
+  if not DEBUG:
+    print("Confirm Rename")
+    input()
+  if NUM_DIR:
+    numberedDirectory()
+  elif REMOVE_PERIODS:
+    removePeriods()
+  else:
+    default()
 
 
 if __name__ == "__main__":
-    main()
+  main()
