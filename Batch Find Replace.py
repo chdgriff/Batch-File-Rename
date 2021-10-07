67a3249a6b93f650b46aa9b2c63449b600e5d9d3
Christopher Griffis <chrisgriffis@gmail.com>
2021-10-06 17:27:13 -0700

    Cleaned up extra code and added commments

--- Batch Find Replace.py
+++ Batch Find Replace.py
@@ -1,22 +1,24 @@
-import os
-from os import path
-import string
-import sys, getopt
+import os, string, sys, getopt
 
 # This is a python script to find and replace file names in a
 # given directory.
 
-DIRECTORY       = "D:\Anime\Baki The Grappler\Season 01"
+# To use input the correct directory and options within the macros below.
+
+DIRECTORY       = "D:\Anime\Baki The Grappler\Season 01"          #Make sure to properly escape \N or \t to \\N and \\t respectively
 FIND            = "Baki The Grappler "
+OFFSET          = 0                         # Additonal characters to find and replace
 REPLACE         = "Baki the Grappler "
+
+# In case where file name is divided by tokens i.e a.file.name.pdf or a_file_name.pdf
 REMOVE_TOKENS   = False
 TOKEN           = '_'
 
-OFFSET          = 0          # Additonal characters to replace
+
 
 # number of files to be renamed
 NUM_START       = 1
-NUM_END         = 0          # inclusive
+NUM_END         = 0          # inclusive, default is 0 for all files found
 
 DEBUG           = True          # Default behavior requiring "-w" flag to write filenames
 
@@ -32,12 +34,15 @@ def fixPath(path):
       newPath += c
   return newPath
 
+
+# Checks if directory exists
 def checkDirectory(path):
   if not os.path.isdir(path):
     print("ERROR: \""+path+"\" cannot be found")
     exit()
   return True
 
+# Checks for correct command line arguments
 def getArgs():
   try:
     opts, args = getopt.getopt(sys.argv[1:], "wt")
@@ -49,6 +54,7 @@ def getArgs():
       global DEBUG
       DEBUG = False
 
+# main function that runs the overall find and replace function.
 def batchFileFindandReplace(n=""):
   introMessage()
   fileDir = DIRECTORY + n
@@ -79,77 +85,6 @@ def batchFileFindandReplace(n=""):
     index += 1
   return filesRenamed
 
-def baki(n=""):
-  fileDir = DIRECTORY + n
-  filesRenamed = 0
-  checkDirectory(fileDir)
-  print("\nDirectory: \""+fileDir+"\"\n")
-
-  index = NUM_START
-  for fileName in os.listdir(fileDir):
-    if NUM_END and index > NUM_END:
-      break
-    foundIndex = fileName.find(FIND)
-    if foundIndex == -1:                 # If not found
-      continue
-    nextChar = foundIndex + len(FIND) + 10
-    if nextChar >= len(fileName):   # If text found at the end of the filename
-      newName = fileName[:foundIndex] + REPLACE
-    else:
-      newName = fileName[:foundIndex] + REPLACE + fileName[nextChar:]
-
-    if not DEBUG:
-      os.rename(fileDir+"/"+fileName, fileDir+"/"+newName)
-    print(fileName + " --> " + newName)
-    filesRenamed += 1
-    index += 1
-  return filesRenamed
-
-
-
-  
-
-def introMessage(opt="default"):
-  print("\n================================================================================")
-  
-  if opt == "token":
-    print("             Starting Batch Token Remover")
-  elif opt == "default":
-    print("             Starting Batch File Name Find and Replace")
-  print("================================================================================")
-  
-
-
-def endingMessage(filesRenamed=0):
-  print()
-  if filesRenamed == 0:
-    print("No files found with \""+FIND+"\" Did you mean something else?")
-  else:
-    if DEBUG:
-      print(str(filesRenamed) + " files to be renamed")
-    else:
-      print(str(filesRenamed) + " files renamed")
-  print("================================================================================\n")
-
-def default():
-  filesRenamed = batchFileFindandReplace()
-  endingMessage(filesRenamed)
-  
-
-def numberedDirectory():
-  totalFilesRenamed = 0
-  for num in range(NUM_START, NUM_END+1):
-    if NUM_END > 9:
-      number = str(num).zfill(2)
-    else:
-      number = str(num)
-    filesRenamed = batchFileFindandReplace(number)
-    endingMessage(filesRenamed)
-    print("\n")
-    totalFilesRenamed += filesRenamed
-  print(str(totalFilesRenamed)+" files renamed in total")
-  print("================================================================================\n")
-
 def removeToken(token = '.'):
   introMessage("token")
   filesRenamed = 0
@@ -167,7 +102,43 @@ def removeToken(token = '.'):
       os.rename(fileDir+"/"+fileName, fileDir+"/"+newName)
     print(fileName + " --> " + newName)
     filesRenamed += 1
-  endingMessage(filesRenamed)
+  return filesRenamed
+
+def introMessage(opt="default"):
+  print("\n======================================================================================")
+  
+  if opt == "token":
+    print("             Starting Batch Token Remover")
+  elif opt == "default":
+    print("             Starting Batch File Name Find and Replace")
+  print("======================================================================================")
+  
+
+def endingMessage(filesRenamed=0):
+  print()
+  if filesRenamed == 0:
+    print("No files found with \""+FIND+"\" Did you mean something else?")
+  else:
+    if DEBUG:
+      print(str(filesRenamed) + " files to be renamed")
+    else:
+      print(str(filesRenamed) + " files renamed")
+  print("======================================================================================\n")
+  
+
+def numberedDirectory():
+  totalFilesRenamed = 0
+  for num in range(NUM_START, NUM_END+1):
+    if NUM_END > 9:
+      number = str(num).zfill(2)
+    else:
+      number = str(num)
+    filesRenamed = batchFileFindandReplace(number)
+    endingMessage(filesRenamed)
+    print("\n")
+    totalFilesRenamed += filesRenamed
+  print(str(totalFilesRenamed)+" files renamed in total")
+  print("===================================================================================\n")
 
 
 def main():
@@ -179,10 +150,12 @@ def main():
     input()
   if NUM_DIR:
     numberedDirectory()
-  elif REMOVE_TOKENS:
-    removeToken(TOKEN)
   else:
-    default()
+    if REMOVE_TOKENS:
+      filesRenamed = removeToken(TOKEN)
+    else:
+      filesRenamed = batchFileFindandReplace()
+    endingMessage(filesRenamed)
 
 
 if __name__ == "__main__":
