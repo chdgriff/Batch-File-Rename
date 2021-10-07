3173f4812812252c47bc1d75b15809a68eaa187e
Christopher Griffis <chrisgriffis@gmail.com>
2021-10-06 18:50:31 -0700

    Converted indentation to spaces

--- Adjust File Numbers.py
+++ Adjust File Numbers.py
@@ -23,119 +23,119 @@ NUM_START       = 1
 NUM_END         = 18              # Inclusive
 
 def batchNumAdjust(n=""):
-	fileDir = DIRECTORY + n
-	filesRenamed = 0
+  fileDir = DIRECTORY + n
+  filesRenamed = 0
 
-	if not os.path.isdir(fileDir):
-		print("ERROR: \""+fileDir+"\" cannot be found")
-		exit()
-	print("\nDirectory: \""+fileDir+"\"\n")
+  if not os.path.isdir(fileDir):
+    print("ERROR: \""+fileDir+"\" cannot be found")
+    exit()
+  print("\nDirectory: \""+fileDir+"\"\n")
 
-	index = 0
-	oldFINDGen = oldSubstring(FIND)
-	newFINDGen = newSubstring(FIND)
-	foundIndex = -1
-	oldFIND = next(oldFINDGen)
-	for fileName in os.listdir(fileDir):
-		if FILE_COUNT and index >= FILE_COUNT:
-			break
-		if foundIndex != -1:
-			oldFIND = next(oldFINDGen)
-		# print(fileName)
-		# print(oldFIND)
-		foundIndex = fileName.find(oldFIND)
-		if foundIndex == -1:                 # If not found
-			continue
+  index = 0
+  oldFINDGen = oldSubstring(FIND)
+  newFINDGen = newSubstring(FIND)
+  foundIndex = -1
+  oldFIND = next(oldFINDGen)
+  for fileName in os.listdir(fileDir):
+    if FILE_COUNT and index >= FILE_COUNT:
+      break
+    if foundIndex != -1:
+      oldFIND = next(oldFINDGen)
+    # print(fileName)
+    # print(oldFIND)
+    foundIndex = fileName.find(oldFIND)
+    if foundIndex == -1:                 # If not found
+      continue
 
-		nextChar = foundIndex + len(FIND)# Finds the next character after FIND
-		newFIND = next(newFINDGen)
+    nextChar = foundIndex + len(FIND)# Finds the next character after FIND
+    newFIND = next(newFINDGen)
 
-		if nextChar >= len(fileName):   # If found at the end of the name
-			newName = fileName[:foundIndex] + newFIND
-		else:
-			newName = fileName[:foundIndex] + newFIND + fileName[nextChar:]
+    if nextChar >= len(fileName):   # If found at the end of the name
+      newName = fileName[:foundIndex] + newFIND
+    else:
+      newName = fileName[:foundIndex] + newFIND + fileName[nextChar:]
 
-		if not DEBUG:
-			os.rename(fileDir+"/"+fileName, fileDir+"/"+newName)
-		print(fileName + " --> " + newName)
-		filesRenamed += 1
-		index += 1
-	return filesRenamed
+    if not DEBUG:
+      os.rename(fileDir+"/"+fileName, fileDir+"/"+newName)
+    print(fileName + " --> " + newName)
+    filesRenamed += 1
+    index += 1
+  return filesRenamed
 
 # Requires '#' to be at end of FIND
 def findDigitCount(subString):
-	digits = 0
-	for i in range(len(subString)-1,-1,-1):
-		if subString[i] == '#':
-			digits += 1
-		else:
-			break
-	return digits
+  digits = 0
+  for i in range(len(subString)-1,-1,-1):
+    if subString[i] == '#':
+      digits += 1
+    else:
+      break
+  return digits
 
 def oldSubstring(startFileName: str)-> str:
-	digits = findDigitCount(startFileName)
-	i = NUM_START
-	while True:
-		yield startFileName[:len(startFileName)-digits]+str(i).zfill(digits)
-		i+=1
+  digits = findDigitCount(startFileName)
+  i = NUM_START
+  while True:
+    yield startFileName[:len(startFileName)-digits]+str(i).zfill(digits)
+    i+=1
 
 def newSubstring(startFileName: str)-> str:
-	digits = findDigitCount(startFileName)
-	i = NUM_START
-	while True:
-		yield startFileName[:len(startFileName)-digits]+str(i).zfill(digits)+" "+str(i+ADJUST)
-		i+=1
+  digits = findDigitCount(startFileName)
+  i = NUM_START
+  while True:
+    yield startFileName[:len(startFileName)-digits]+str(i).zfill(digits)+" "+str(i+ADJUST)
+    i+=1
 
 def introMessage(s=""):
-	print("\n======================================================================================")
-	print("                Starting Batch File Name Find and Replace")
-	print("======================================================================================")
+  print("\n======================================================================================")
+  print("                Starting Batch File Name Find and Replace")
+  print("======================================================================================")
 
 
 def endingMessage(filesRenamed=0):
-	print()
-	if filesRenamed == 0:
-		print("No files found with \""+FIND+"\" Did you mean something else?")
-	else:
-		if DEBUG:
-			print(str(filesRenamed) + " files to be renamed")
-		else:
-			print(str(filesRenamed) + " files renamed")
+  print()
+  if filesRenamed == 0:
+    print("No files found with \""+FIND+"\" Did you mean something else?")
+  else:
+    if DEBUG:
+      print(str(filesRenamed) + " files to be renamed")
+    else:
+      print(str(filesRenamed) + " files renamed")
 
 def default():
-	filesRenamed = batchNumAdjust()
-	endingMessage(filesRenamed)
-	print("======================================================================================\n")
+  filesRenamed = batchNumAdjust()
+  endingMessage(filesRenamed)
+  print("======================================================================================\n")
 
 def numberedDirectory():
-	totalFilesRenamed = 0
-	for num in range(NUM_START, NUM_END+1):
-		if NUM_END > 9:
-			number = str(num).zfill(2)
-		else:
-			number = str(num)
-		filesRenamed = batchNumAdjust(number)
-		endingMessage(filesRenamed)
-		print("\n")
-		totalFilesRenamed += filesRenamed
-	print(str(totalFilesRenamed)+" files renamed in total")
-	print("======================================================================================\n")
+  totalFilesRenamed = 0
+  for num in range(NUM_START, NUM_END+1):
+    if NUM_END > 9:
+      number = str(num).zfill(2)
+    else:
+      number = str(num)
+    filesRenamed = batchNumAdjust(number)
+    endingMessage(filesRenamed)
+    print("\n")
+    totalFilesRenamed += filesRenamed
+  print(str(totalFilesRenamed)+" files renamed in total")
+  print("======================================================================================\n")
 
 def main():
-	opts, args = getopt.getopt(sys.argv[1:], "wt")
-	for opt, arg in opts:
-		if opt == '-w':
-			global DEBUG
-			DEBUG = False
-	introMessage()
-	if not DEBUG:
-		print("Confirm Rename")
-		input()
-	if NUM_DIR:
-		numberedDirectory()
-	else:
-		default()
+  opts, args = getopt.getopt(sys.argv[1:], "wt")
+  for opt, arg in opts:
+    if opt == '-w':
+      global DEBUG
+      DEBUG = False
+  introMessage()
+  if not DEBUG:
+    print("Confirm Rename")
+    input()
+  if NUM_DIR:
+    numberedDirectory()
+  else:
+    default()
 
 
 if __name__ == "__main__":
-	main()
+  main()
--- Batch Prepend Append.py
+++ Batch Prepend Append.py
@@ -23,87 +23,87 @@ FILE_COUNT = 0              # inclusive (default 0 means all files found)
 DEBUG = True              # change default behavior from requiring '-w' to write filenames
 
 def checkDirectory(path):
-	if not os.path.isdir(path):
-		print("ERROR: \""+path+"\" cannot be found")
-		exit()
-	return True
+  if not os.path.isdir(path):
+    print("ERROR: \""+path+"\" cannot be found")
+    exit()
+  return True
 
 def fixPath(path):
-	newPath = ''
-	for c in path:
-		if c == '\\':
-			newPath += '\\\\'
-		else:
-			newPath += c
-	return newPath
+  newPath = ''
+  for c in path:
+    if c == '\\':
+      newPath += '\\\\'
+    else:
+      newPath += c
+  return newPath
 
 def batchPrependAppend(s=""):
-	fileDir = DIRECTORY + s
-	filesRenamed = 0
-	if DEBUG:
-		print("--------------------------------DEBUG--------------------------------")
-	checkDirectory(fileDir)
-	print("\nDirectory: \""+fileDir+"\"\n")
-	index = 0
-	for fileName in os.listdir(fileDir):
-		if FILE_COUNT and index >= FILE_COUNT:
-			break
+  fileDir = DIRECTORY + s
+  filesRenamed = 0
+  if DEBUG:
+    print("--------------------------------DEBUG--------------------------------")
+  checkDirectory(fileDir)
+  print("\nDirectory: \""+fileDir+"\"\n")
+  index = 0
+  for fileName in os.listdir(fileDir):
+    if FILE_COUNT and index >= FILE_COUNT:
+      break
 
-		if NUM:
-			textToAdd = NUM_L+str(index).zfill(DIGITS)+NUM_R
-		else:
-			textToAdd = TEXT
+    if NUM:
+      textToAdd = NUM_L+str(index).zfill(DIGITS)+NUM_R
+    else:
+      textToAdd = TEXT
 
-		if PREPEND_OR_APPEND == 'A':
-			if APPEND_FILE_EXT:
-				newName = fileName+textToAdd
-			else:
-				newName = os.path.splitext(fileName)[0]+textToAdd+os.path.splitext(fileName)[1]
-		else:
-			newName = textToAdd+fileName
+    if PREPEND_OR_APPEND == 'A':
+      if APPEND_FILE_EXT:
+        newName = fileName+textToAdd
+      else:
+        newName = os.path.splitext(fileName)[0]+textToAdd+os.path.splitext(fileName)[1]
+    else:
+      newName = textToAdd+fileName
 
-		if not DEBUG:
-			os.rename(fileDir+"/"+fileName, fileDir+"/"+newName)
-		print(fileName + " --> " + newName)
-		filesRenamed += 1
-		index += 1
-	return filesRenamed
+    if not DEBUG:
+      os.rename(fileDir+"/"+fileName, fileDir+"/"+newName)
+    print(fileName + " --> " + newName)
+    filesRenamed += 1
+    index += 1
+  return filesRenamed
 
 def introMessage(s=""):
-	print("===========================================================================")
-	print("             Starting Batch File Prepend or Append")
-	print("===========================================================================")
+  print("===========================================================================")
+  print("             Starting Batch File Prepend or Append")
+  print("===========================================================================")
 
 
 def endingMessage(filesRenamed=0):
-	print()
-	if filesRenamed == 0:
-		print("No files found with \""+FIND+"\" Did you mean something else?\n")
-	else:
-		if DEBUG:
-			print(str(filesRenamed) + " files to be renamed")
-		else:
-			print(str(filesRenamed) + " files renamed")
+  print()
+  if filesRenamed == 0:
+    print("No files found with \""+FIND+"\" Did you mean something else?\n")
+  else:
+    if DEBUG:
+      print(str(filesRenamed) + " files to be renamed")
+    else:
+      print(str(filesRenamed) + " files renamed")
 
 def default():
-	filesRenamed = batchPrependAppend()
-	endingMessage(filesRenamed)
-	print("===========================================================================\n")
+  filesRenamed = batchPrependAppend()
+  endingMessage(filesRenamed)
+  print("===========================================================================\n")
 
 def main():
-	global DIRECTORY
-	#DIRECTORY = fixPath(DIRECTORY)
+  global DIRECTORY
+  #DIRECTORY = fixPath(DIRECTORY)
 
-	opts, args = getopt.getopt(sys.argv[1:], "wt")
-	for opt, arg in opts:
-		if opt == '-w':
-			global DEBUG
-			DEBUG = False
-			print("Confirm Rename")
-			input()
-	introMessage()
-	default()
+  opts, args = getopt.getopt(sys.argv[1:], "wt")
+  for opt, arg in opts:
+    if opt == '-w':
+      global DEBUG
+      DEBUG = False
+      print("Confirm Rename")
+      input()
+  introMessage()
+  default()
 
 
 if __name__ == "__main__":
-		main()
+  main()
