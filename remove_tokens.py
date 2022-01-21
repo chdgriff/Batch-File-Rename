14f5dfcac2ecf16e73fc5d05a03f331ac0ccd05e
Christopher Griffis <chrisgriffis@gmail.com>
2022-01-21 07:00:53 -0800

    Created seperate remove_tokens module

---
+++ remove_tokens.py
@@ -0,0 +1,12 @@
+class RemoveTokens():
+  def __init__(self, file_path, token) -> None:
+      self.file_path = file_path
+      self.token = token
+      self.log = []
+
+  def removeTokens():
+    pass
+  def intro_message(self):
+    self.log.append("\n=====================================================================================")
+    self.log.append("{:^90s}".format("Starting Batch File Name Find and Replace"))
+    self.log.append("=====================================================================================")
