25446b15fbf3bbfe6b27c3c0a063366e604abb5e
Christopher Griffis <chrisgriffis@gmail.com>
2022-02-15 23:26:01 -0800

    Added docstrings

--- core.py
+++ core.py
@@ -1,15 +1,37 @@
 import os
 
 class BatchFileNameOperations():
-  def __init__(self, file_path, file_count, debug=True) -> None:
-      self.file_path = file_path
-      self.file_count = file_count
-      self.debug = debug
-      self.log = []
-      self.error_msg = ""
+  """
+  Contains core functions and variables.
+
+
+  -----------
+  Attributes:
+  -----------
+  file_path : str
+    The path to files.
+  file_count : int
+    The number of files to run the operations on.
+  debug : bool
+    Whether to just print the log or write out file name operations (default True).
   
-  def check_core_vars(self):
-    if not self.check_file_path():
+  --------
+  Methods:
+  --------
+  check_core_vars() --> bool
+    Runs error checking on the core variables.
+  stringify_log() --> str
+  """
+  def __init__(self, file_path, file_count, debug=True) -> None:
+    self.file_path = file_path
+    self.file_count = file_count
+    self.debug = debug
+    self.log = []
+    self.error_msg = ""
+  
+  def check_core_vars(self) -> bool:
+    """Runs error checking on the core variables."""
+    if not self._check_file_path():
       self.error_msg = "ERROR: \""+self.file_path+"\" cannot be found"
       return False
     if self.file_count < 0:
@@ -17,10 +39,12 @@ class BatchFileNameOperations():
       return False
     return True
 
-  def stringify_log(self):
+  def stringify_log(self) -> str:
+    """Converts log from list to string"""
     log = '\n'.join(self.log)
     print(log)
     return log
 
-  def check_file_path(self):
+  def _check_file_path(self) -> bool:
+    """Checks if directory exists"""
     return os.path.isdir(self.file_path)
