df38c418c26ee146e793e47d0869ac2ac7a36891
Christopher Griffis <chrisgriffis@gmail.com>
2022-02-15 23:49:58 -0800

    Added docstrings and comments

--- batch_find_replace.py
+++ batch_find_replace.py
@@ -3,6 +3,27 @@ from Ignored_Files import file_list as ignored_files
 from core import *
 
 class BatchFindReplace(BatchFileNameOperations):
+  """
+  Runs find and replace operations on filenames in a given directory.
+
+  -----------
+  Attributes:
+  -----------
+  find_text : str
+  offset    : int
+    How much extra to replace.
+  replace_text  : str
+  ignore_case   : bool
+    Whether to ignore case in searching for find_text.
+  
+  --------
+  Methods:
+  --------
+  run() -> str
+    Runs overall operations.
+  check_vars() -> bool
+    Error checks the attributes.
+  """
   def __init__(self, dir_path, find_text, offset, replace_text, ignore_case, file_count, debug=True):
     super().__init__(dir_path, file_count, debug)
     self.offset = offset
@@ -10,22 +31,23 @@ class BatchFindReplace(BatchFileNameOperations):
     self.ignore_case = ignore_case
     self.find_text = find_text.upper() if self.ignore_case else find_text
 
-  def run(self):
+  def run(self) -> str:
+    """Runs overall operations."""
     if not self.check_vars(): return self.error_msg
-    self.find_and_replace()
+    self._find_and_replace()
     return super().stringify_log()
 
-  # Checks variables for valid values
-  def check_vars(self):
+  def check_vars(self) -> bool:
+    """Error checks attributes."""
     if not super().check_core_vars(): return False
     if self.offset < 0:
       self.error_msg = "ERROR: offset cannot be less than zero"
       return False
     return True
 
-  # main function that runs the overall find and replace function.
-  def find_and_replace(self, n=""):
-    self.intro_message()
+  def _find_and_replace(self, n=""):
+    """"""
+    self._intro_message()
     file_directory = self.dir_path + n
     files_renamed = 0
     
@@ -39,31 +61,33 @@ class BatchFindReplace(BatchFileNameOperations):
       found_idx = (file_name.upper() if self.ignore_case else file_name).find(self.find_text)
       if found_idx == -1: continue
 
-
-      file_extension_idx = file_name.rfind('.')
-      nextChar = found_idx + len(self.find_text) + self.offset # Finds the next character after text
-      if nextChar > file_extension_idx: nextChar = file_extension_idx
+      # Finds the next character after text
+      nextChar = found_idx + len(self.find_text) + self.offset 
       
-  
+      # Checks if offset would overwrite filename extension
+      file_extension_idx = file_name.rfind('.')
+      if nextChar > file_extension_idx: nextChar = file_extension_idx
+
+      # Creates the new file name
       if nextChar >= len(file_name):   # If text found at the end of the filename
         new_name = file_name[:found_idx] + self.replace_text
       else:
         new_name = file_name[:found_idx] + self.replace_text + file_name[nextChar:]
 
-      if not self.debug:
+      if not self.debug: # Writes out new file name if not debug
         os.rename(file_directory+"/"+file_name, file_directory+"/"+new_name)
       self.log.append(file_name + " --> " + new_name)
       files_renamed += 1
       index += 1
-    self.end_message(files_renamed)
+    self._end_message(files_renamed)
 
-  def intro_message(self):
+  def _intro_message(self):
     self.log.append("\n=====================================================================================")
     self.log.append("{:^90s}".format("Starting Batch File Name Find and Replace"))
     self.log.append("=====================================================================================")
   
 
-  def end_message(self, files_renamed=0):
+  def _end_message(self, files_renamed=0):
     if files_renamed == 0:
       self.log.append("\nNo files found with \""+self.find_text+"\" Did you mean something else?")
     else:
